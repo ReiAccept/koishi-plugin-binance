024c7fcb74080ba77a4fad787a0bcb6a99009c34
Space Li <lhh@live.com>
2021-08-11 17:05:17 +0800

    Use yarn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ module.exports = (ctx) => {
         .action(async (_, type) => {
             type = type.toUpperCase() + 'USDT'
             const f = (t) => Math.floor(t * 1000) / 1000
-            const { body } = await require('superagent').get('https://api-binance.vercel.app/api/v3/ticker/24hr?symbol=' + type)
-            return `${type}-USDT
-最新价格 ${f(body.lastPrice)}(${f(body.priceChange)})
+            const { body } = await require('superagent').get('https://api.binance.com/api/v3/ticker/24hr?symbol=' + type)
+            return `${type}
+最新价格 ${f(body.lastPrice)}(价格变化 ${f(body.priceChange)})
 24h最高 ${f(body.highPrice)}
 24h最低 ${f(body.lowPrice)}`
         })
